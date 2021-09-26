50a731b9605bc0def59c9cf11d2cea3cad4a20af
devtousif <tousifrahmanofficial21@.com>
2021-09-26 23:23:18 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Suitcontainer from "./components/Suitcontainer/Suitcontainer";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Suitcontainer></Suitcontainer>
+    </div>
+  );
+}
+
+export default App;
